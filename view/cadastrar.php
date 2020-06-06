0e95d98fcccc47e7be678060f72badb70a5069d5
Marlon Santana dos Santos <marlonfms2012@gmail.com>
2020-06-06 10:40:00 -0300

    Update cadastrar.php

--- view/cadastrar.php
+++ view/cadastrar.php
@@ -5,4 +5,15 @@
             include("head.php");
         ?>
     </head>
+    <body>
+        <div class="formulários">
+
+            <h2>Preecha os seus dados</h2>
+            <form action="POST" ></form>
+            <div class="formgroup">
+
+            </div>
+
+        </div>
+    </body>
 </html>
