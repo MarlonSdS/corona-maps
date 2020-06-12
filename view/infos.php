3b5a90988a30f3709c23f47bcc8e8da5e33e8672
Marlon Santana dos Santos <marlonfms2012@gmail.com>
2020-06-12 10:29:41 -0300

    Inicio da tela de informações

---
+++ view/infos.php
@@ -0,0 +1,39 @@
+<!Doctype html>
+<html lang="pt-br">
+
+    <head>
+        <title>Corona Maps</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../view/styles/bootstrap.css">
+        <link rel="stylesheet" href="../view/styles/infos.css">
+    </head>
+    <body>
+        <header>
+            <h1> </h1>
+                    <!-- Menu -->
+            <div class="Menu">
+                <input type="checkbox" id="chec">
+                <label for="chec">
+                    <img src="imagens/menu.png">
+                </label>
+
+                <nav>
+                    <ul>
+                        <li><b><a href="../index.html">Inicio</a></b></li>
+                        <li><b><a href="infos,php">Regional</a></b></li>
+                        <li><b><a href="/login.php">Entrar</a></b></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+
+        <main>
+            <form action="">
+                <input type="">
+            </form>
+        </main>
+
+        <footer></footer>
+    </body>
+
+</html>
